3a453224f1d9ed8d13aa35e773fb8e6580370d00
Akibrot <akibrotsamuelas@gmail.com>
2022-10-10 10:30:29 +0300

    final

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import IP from "ip";
 import {
   createProject,
   createUser,
@@ -15,20 +14,15 @@ import {
 const Routes = express.Router();
 
 Routes.get("/welcome", welcome);
-Routes.get("/", (req,res)=>{
-  res.send("server startted")
+Routes.get("/", (req, res) => {
+  res.send("server startted");
 });
 Routes.post("/createUser", createUser);
 Routes.get("/getUsers", getUsers);
 Routes.get("/getProjects", getProjects);
 Routes.post("/createProject", createProject);
 Routes.post("/createTask", createTask);
-Routes.post("/deleteProject",deleteProject);
-Routes.post("/getTask",getTask);
+Routes.post("/deleteProject", deleteProject);
+Routes.post("/getTask", getTask);
 
-
-Routes.get("/", (req, res) => {
-  const ipAddress = IP.address();
-  res.send(ipAddress);
-});
 export default Routes;
