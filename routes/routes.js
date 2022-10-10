c03dbe256457db8c663f5598712c2f16f1145c39
Akibrot <akibrotsamuelas@gmail.com>
2022-10-10 09:30:24 +0300

    second

--- routes/routes.js
+++ routes/routes.js
@@ -13,7 +13,11 @@ import {
 } from "../controllers/controllers.js";
 
 const Routes = express.Router();
+
 Routes.get("/welcome", welcome);
+Routes.get("/", (req,res)=>{
+  res.send("server startted")
+});
 Routes.post("/createUser", createUser);
 Routes.get("/getUsers", getUsers);
 Routes.get("/getProjects", getProjects);
